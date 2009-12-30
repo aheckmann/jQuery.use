6951df375c74b5f129409d3aaffaba6fdaea700b
Aaron Heckmann <aaron.heckmann@gmacfs.com>
2009-12-30 12:23:24 -0500

    added PHP package service stub file
    Signed-off-by: Aaron Heckmann <aaron.heckmann@gmacfs.com>

---
+++ pkg/index.php
@@ -0,0 +1,11 @@
+<?php
+// a starter version of a package service
+// You should probably rewrite this since I'm no PHP expert
+
+// parse params in order
+// loop through and get files if they are on the whitelist
+// cache the resulting concated files server side
+// set far future expires headers
+// output the concatenated result
+
+?>
