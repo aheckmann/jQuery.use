a0874b108c4a77d3e7cc6fa60fa8eff6bf9132ef
Aaron Heckmann <aaron.heckmann@gmacfs.com>
2010-01-04 16:29:52 -0500

    added caching to PHP pkg script
    Signed-off-by: Aaron Heckmann <aaron.heckmann@gmacfs.com>

--- pkg/index.php
+++ pkg/index.php
@@ -4,37 +4,63 @@
 * You should probably rewrite this since I'm no PHP expert
 */
 
-
-$output = '';
-
+$cachedir = dirname(__FILE__) . '/cache';
+$validFiles = array();
 
 // put params in order, maybe not necessary but just in case
 ksort($_GET, SORT_STRING);
 
-
-// ensure files are cached client side
-$expires = 60*60*24*365*10; // 10 yrs
-header('Expires: '. gmdate( 'D, d M Y H:i:s', time() + $expires ) .' GMT');
-
-
-$contentType = endsWith( current($_GET), '.js') ? 'text/javascript' : 'text/css';	
-header("Content-Type: " . $contentType);
-
-
-$pathToPkgAssets = '../lib/1-0-0/';
-foreach( $_GET as $requestedFile) {	
+// determine valid file requests
+foreach( $_GET as $requestedFile ) {	
 	if ( inWhiteList($requestedFile) ) {
-		$contents = file_get_contents($pathToPkgAssets . $requestedFile);	
-		if ($contents) {
-			$output .= $contents;
-		}
-		
+		$validFiles[] = $requestedFile;
+	}
+}
+
+// create unique filename based on valid file requests
+// we use this for caching to disk
+$cachefile = md5(implode('#', $validFiles));
+$cachefullpath = $cachedir .'/'. $cachefile;
+
+$expires = 60*60*24*365*10; // 10 yrs
+$contentType = endsWith(current($_GET), '.js') ? 'text/javascript' : 'text/css';
+
+
+if ( file_exists($cachefullpath) ) {
+	if ( $fp = fopen($cachefullpath, 'rb') ) {
+		header("Content-Encoding: gzip");	
+		header("Content-Type: " . $contentType);
+		header("Content-Length: " . filesize($cachefullpath));
+		header('Expires: '. gmdate( 'D, d M Y H:i:s', time() + $expires ) .' GMT');
+		header('Cache-Control: max-age=' . $expires);
+		fpassthru($fp);
+		fclose($fp);
+		exit;
 	}
 }
 
 
-// TODO - cache the resulting concated files server side
+$output = '';
+$pathToPkgAssets = '../lib/1-0-0/';
+foreach( $validFiles as $validFile ) {	
+	$output .= file_get_contents($pathToPkgAssets . $validFile);	
+}
+
+	
+$output = gzencode($output, 9, FORCE_GZIP);
+header("Content-Encoding: gzip");
+header("Content-Type: " . $contentType);
+header('Content-Length: ' . strlen($output));
+header('Expires: '. gmdate( 'D, d M Y H:i:s', time() + $expires ) .' GMT');
+header('Cache-Control: max-age=' . $expires);
 echo $output;
+
+
+// store the file in cache
+if ($fp = fopen($cachefullpath, 'wb')) {
+	fwrite($fp, $output);
+	fclose($fp);
+}
 
 
 
